f5e1d7717846a4365d4560a50855ec03d9afbd48
alejocorti <acortigiganti@udesa.edu.ar>
2023-04-04 16:10:28 -0300

    Probando Git y empezando rutas y controladores

---
+++ miproyecto/routes/index.js
@@ -0,0 +1,11 @@
+const express = requiere ("express");
+const router = express.Router();
+const controller = require('../controller/indexController')
+
+
+/* GET home page. */ 
+
+router.get('/login', function(req,res){});
+
+
+module.exports = router;
