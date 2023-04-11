b86bf9f814679ae348a98da9d6d7cc44511cd16d
alejocorti <acortigiganti@udesa.edu.ar>
2023-04-11 15:52:12 -0300

    Vistas

--- miproyecto/data/data.js
+++ miproyecto/data/data.js
@@ -10,64 +10,64 @@ const data = {
     },
     productos: [
         {
-            nombre: 'Mate Torpedo',
+            nombre: 'Mate Torpedo Alpaca',
             foto: '/images/products/torpedo_alpaca_2.png',
             descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
             fecha_de_carga: '09/12/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Mate Torpedo Acero',
+            foto: '/images/products/torpedo3.png',
+            descripcion: 'Torpedo de cuero y virola de acero, costura uruguaya',
+            fecha_de_carga: '02/12/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Mate Torpedo Croco',
+            foto: '/images/products/torpedo4.png',
+            descripcion: 'Torpedo de cuero y virola croco, costura uruguaya',
+            fecha_de_carga: '04/12/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Mate Imperial',
+            foto: '/images/products/imperial.png',
+            descripcion: 'Mate imperial de alpaca, costura uruguaya',
+            fecha_de_carga: '23/12/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombilla Loro',
+            foto: '/images/products/bombilla1.png',
+            descripcion: 'Bombilla Pico de loro de alpaca',
+            fecha_de_carga: '09/10/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombillon',
+            foto: '/images/products/bombilla2.png',
+            descripcion: 'Bombillon Pico de loro de alpaca',
+            fecha_de_carga: '09/11/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombilla Acero',
+            foto: '/images/products/bombilla3.png',
+            descripcion: 'Bombilla Pico de loro de acero',
+            fecha_de_carga: '08/10/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombilla Alpaca',
+            foto: '/images/products/bombilla4.png',
+            descripcion: 'Bombilla Pico ancho de alpaca',
+            fecha_de_carga: '19/11/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombilla Rey',
+            foto: '/images/products/bombilla5.png',
+            descripcion: 'Bombilla Pico de rey de alpaca',
+            fecha_de_carga: '12/12/2018'
         },
         {
-            nombre: 'Mate Torpedo',
-            foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
-            fecha_de_carga: '09/12/2018'
+            nombre: 'Bombilla Bor',
+            foto: '/images/products/bombilla6.png',
+            descripcion: 'Bombilla Pico Bor de alpaca',
+            fecha_de_carga: '14/12/2018'
         },
         
     ],
