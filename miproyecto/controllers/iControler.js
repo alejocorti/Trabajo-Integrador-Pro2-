44e14b06ecd87032e2beee8ee9cdb613f9f4f56b
fedevitale1 <favitale@udesa.edu.ar>
2023-04-12 15:59:08 -0300

    index controller problema de usuario visible solucionado

--- miproyecto/controllers/iControler.js
+++ miproyecto/controllers/iControler.js
@@ -3,7 +3,7 @@ const data = require('../data/data')
 const controller = {
     index: function (req, res) {
         if(req.params.si){
-            return res.render('index', {lsProd: data.productos, logueado: 'si', usuarios: data.usuarios.usuarios})
+            return res.render('index', {lsProd: data.productos, logueado: 'si', usuario: data.usuarios.usuario})
         }
         else{
            return res.render('index', {lsProd: data.productos, logueado: 'no'})
