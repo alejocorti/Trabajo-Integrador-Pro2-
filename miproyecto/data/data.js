ab8db6f8a7be132ca78bdf0e92359ca69b2f0180
fedevitale1 <favitale@udesa.edu.ar>
2023-04-12 16:09:01 -0300

    modificaciones modulo de datos

--- miproyecto/data/data.js
+++ miproyecto/data/data.js
@@ -1,10 +1,10 @@
 const data = {
     usuarios:{
-        mail: 'alejocortigiganti@gmail.com',
-        usuario: 'Cortipopo08',
-        contrasena: 'Cortigiganti',
-        fecha_de_nacimiento: '02/09/2003',
-        doc: '44842994',
+        mail: 'luisnavas@gmail.com',
+        usuario: 'Luis_navas10',
+        contrasena: 'quelindoprogramar',
+        fecha_de_nacimiento: '02/09/1988',
+        doc: '30842994',
         fto: '/images/users/Logo_Recado_circular.png'
     
     },
@@ -12,61 +12,61 @@ const data = {
         {
             nombre: 'Mate Torpedo Alpaca',
             foto: '/images/products/torpedo_alpaca_2.png',
-            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya',
+            descripcion: 'Torpedo de cuero y virola de alpaca, costura uruguaya.',
             fecha_de_carga: '09/12/2018'
         },
         {
             nombre: 'Mate Torpedo Acero',
             foto: '/images/products/torpedo3.png',
-            descripcion: 'Torpedo de cuero y virola de acero, costura uruguaya',
+            descripcion: 'Torpedo de cuero y virola de acero, costura uruguaya.',
             fecha_de_carga: '02/12/2018'
         },
         {
             nombre: 'Mate Torpedo Croco',
             foto: '/images/products/torpedo4.png',
-            descripcion: 'Torpedo de cuero y virola croco, costura uruguaya',
+            descripcion: 'Torpedo de cuero y virola croco, costura uruguaya.',
             fecha_de_carga: '04/12/2018'
         },
         {
             nombre: 'Mate Imperial',
             foto: '/images/products/imperial.png',
-            descripcion: 'Mate imperial de alpaca, costura uruguaya',
+            descripcion: 'Mate imperial de alpaca, costura uruguaya.',
             fecha_de_carga: '23/12/2018'
         },
         {
             nombre: 'Bombilla Loro',
             foto: '/images/products/bombilla1.png',
-            descripcion: 'Bombilla Pico de loro de alpaca',
+            descripcion: 'Bombilla Pico de loro de alpaca.',
             fecha_de_carga: '09/10/2018'
         },
         {
             nombre: 'Bombillon',
             foto: '/images/products/bombilla2.png',
-            descripcion: 'Bombillon Pico de loro de alpaca',
+            descripcion: 'Bombillon Pico de loro de alpaca.',
             fecha_de_carga: '09/11/2018'
         },
         {
             nombre: 'Bombilla Acero',
             foto: '/images/products/bombilla3.png',
-            descripcion: 'Bombilla Pico de loro de acero',
+            descripcion: 'Bombilla Pico de loro de acero.',
             fecha_de_carga: '08/10/2018'
         },
         {
             nombre: 'Bombilla Alpaca',
             foto: '/images/products/bombilla4.png',
-            descripcion: 'Bombilla Pico ancho de alpaca',
+            descripcion: 'Bombilla Pico ancho de alpaca.',
             fecha_de_carga: '19/11/2018'
         },
         {
             nombre: 'Bombilla Rey',
             foto: '/images/products/bombilla5.png',
-            descripcion: 'Bombilla Pico de rey de alpaca',
+            descripcion: 'Bombilla Pico de rey de alpaca.',
             fecha_de_carga: '12/12/2018'
         },
         {
             nombre: 'Bombilla Bor',
             foto: '/images/products/bombilla6.png',
-            descripcion: 'Bombilla Pico Bor de alpaca',
+            descripcion: 'Bombilla Pico Bor de alpaca.',
             fecha_de_carga: '14/12/2018'
         },
         
@@ -84,12 +84,12 @@ const data = {
         },
         {
             nombre_de_usuario: 'Messias10',
-            comentario: 'despues de la del mundo, esto fue lo mejor que me paso',
+            comentario: 'Despúes de la del mundo, esto fue lo mejor que me pasó :)',
             imagen: '/images/users/messi.jpg'
         },
         {
             nombre_de_usuario: 'Wanchupikemero',
-            comentario: 'Tirate un canje lauchin',
+            comentario: 'Tirate un canje lauchinn',
             imagen: '/images/users/wanchope'
         },
     ]
